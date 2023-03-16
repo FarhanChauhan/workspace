91f8800e02d8f0ab73e0acfaf97f6ef7d60a7df7
FarhanChauhan <farhanchauhan950@gmail.com>
2023-03-16 16:19:41 +0530

    third commit

--- in/ineuron/main/Asssignment2.java
+++ in/ineuron/main/Asssignment2.java
@@ -3,8 +3,8 @@ package in.ineuron.main;
 public class Asssignment2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int [] arr = new int[5];
+		System.out.println(arr);
 	}
 
 }
